78f287e20706f031338715f75912354f1f2f5fb7
Nicolas Nitta <nic360hnitta10@gmail.com>
2024-09-20 16:53:21 -0300

    moldando o controller do usuario

--- api_web/src/main/java/api_web/api_web/controller/UsuarioController.java
+++ api_web/src/main/java/api_web/api_web/controller/UsuarioController.java
@@ -2,7 +2,38 @@ package api_web.api_web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import api_web.api_web.model.Usuario;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+/*    private String nickname;
+    private String email;
+    private String nome;
+    private String descricao;
+    private long seguidores;
+    private String senha; */
+
 @RestController
 public class UsuarioController {
-    
+    ArrayList<Usuario> listaUsuario = new ArrayList<>();
+
+    public UsuarioController() {
+        listaUsuario.add(
+                new Usuario("Mickey", "123@gmail.com", "Samu", "123", 123, "123"));
+        listaUsuario.add(
+                new Usuario("CleitinDoPneu", "job@gmail.com", "Nicolas", "Job", 123, "Job"));
+    }
+
+    }
+
+    @GetMapping("/Usuario")
+    public String getUsuarioByNick(@PathVariable("nickname") String nickname) {
+        return void;
+    }
+
 }
--- api_web/src/main/java/api_web/api_web/model/Usuario.java
+++ api_web/src/main/java/api_web/api_web/model/Usuario.java
@@ -1,10 +1,25 @@
 package api_web.api_web.model;
 
+import lombok.Data;
+
+@Data
+
 public class Usuario {
+
     private String nickname;
     private String email;
     private String nome;
     private String descricao;
     private long seguidores;
     private String senha;
+    
+    public Usuario(String nickname, String email, String nome, String descricao, long seguidores, String senha) {
+        this.nickname = nickname;
+        this.email = email;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.seguidores = seguidores;
+        this.senha = senha;
+    }
+
 }
